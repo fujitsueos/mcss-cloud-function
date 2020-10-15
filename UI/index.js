b78b4722500ddb93b25d37890ca3f2b7845701f6
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-10-15 10:34:54 +0300

    change UI to async

--- UI/index.js
+++ UI/index.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function (context, req) {
+module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger UI function processed a request.');
 
-  var res = {
-    body: '',
+  const htmlFilePath = path.resolve(__dirname, 'index.html');
+  const htmlBody = fs.readFileSync(htmlFilePath, { encoding: 'utf8', flag: 'r' });
+
+  context.res = {
+    status: 200,
+    body: htmlBody,
     headers: {
       'Content-Type': 'text/html',
     },
   };
-
-  // Read an HTML file in the directory and return the contents
-  fs.readFile(path.resolve(__dirname, 'index.html'), 'UTF-8', (err, htmlContent) => {
-    res.body = htmlContent;
-    context.done(null, res);
-  });
 };
