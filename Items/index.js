79f8e76100b45223ad4f204a5802bf31a48c3f83
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-10-16 09:24:07 +0300

    add proxies

---
+++ Items/index.js
@@ -0,0 +1,29 @@
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger Items API function processed a request.');
+
+  const items = [
+    {
+      id: 1,
+      name: 'My item 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Book',
+      price: 25,
+    },
+    {
+      id: 3,
+      name: 'Headset',
+      price: 42,
+    },
+  ];
+
+  context.res = {
+    status: 200,
+    body: items,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+};
