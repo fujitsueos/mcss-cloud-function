fd65648b6d370f545619bfaf3d30e22d3358f821
Sami Koskivaara <sami.koskivaara@gmail.com>
2020-10-19 23:12:36 +0300

    add post method

--- Items/index.js
+++ Items/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger Items API function processed a request.');
-
+  let status = 200;
   const items = [
     {
       id: 1,
@@ -15,12 +15,23 @@ module.exports = async function (context, req) {
     {
       id: 3,
       name: 'Headset',
-      price: 42,
+      price: 45,
     },
   ];
 
+  if (req.method === 'POST') {
+    status = 201;
+    const newItem = {
+      id: Math.floor(Math.random() * 100000) + 1,
+      name: req.body.name,
+      price: req.body.price,
+    };
+
+    items.push(newItem);
+  }
+
   return {
-    status: 200,
+    status,
     body: items,
     headers: {
       'Content-Type': 'application/json',
